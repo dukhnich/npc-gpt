35ae1fc8658f9a310d4a15f35288e3f71de2fd6c
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-04 13:26:36 +0100

    refactor popup

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,56 +1,12 @@
 import styles from "./index.module.css";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import { useEffect, useState } from "react";
+import KeyPopup from "../KeyPopup/index.jsx";
 
-const Header = ({ apiKey, onSend }) => {
-  const [isPopup, setPopupStatus] = useState(false);
-  const [newApiKey, setNewApiKey] = useState(apiKey);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSend(newApiKey);
-  };
-  const togglePopup = () => setPopupStatus(!isPopup);
+const Header = (props) => {
   return (
     <header className={styles.header}>
       <div className={styles.header__logo}>LOGO</div>
       <p className={styles.header__title}>Dialog with NPC</p>
-      <Button variant="primary" onClick={togglePopup}>
-        Add OpenApi Key
-      </Button>
-      <Modal
-        show={isPopup}
-        onHide={togglePopup}
-        dialogClassName="modal-90w"
-        aria-labelledby="example-custom-modal-styling-title"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
-            Your OpenApi Key
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={handleSubmit}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="basic-addon1">🔑</InputGroup.Text>
-              <Form.Control
-                placeholder="OpenApi Key"
-                aria-label="OpenApi Key"
-                aria-describedby="basic-addon1"
-                type="password"
-                value={newApiKey}
-                onChange={(e) => setNewApiKey(e.target.value)}
-              />
-              <Button variant="outline-primary" type="submit">
-                Set Key
-              </Button>
-            </InputGroup>
-          </Form>
-        </Modal.Body>
-      </Modal>
+      <KeyPopup {...props} />
     </header>
   );
 };
